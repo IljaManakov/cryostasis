7f1308e65c9f54cc94c57a7d8c9adfb752325faf
Ilja Manakov <manakov@imfusion.com>
2024-07-09 08:16:44 +0200

    extend freezing to setitem

--- cryo/__init__.py
+++ cryo/__init__.py
@@ -3,18 +3,28 @@ class ImmutableError(Exception):
 
 
 class Frozen:
-
-    def __instancecheck__(cls, instance):
-        print("In Frozen")
-        return cls.__instancecheck__(instance) or instance.__bases__[1].__instancecheck__(instance)
+    __freeze_attribute_assignment = True
+    __freeze_item_assignment = True
 
     def __setattr__(self, name, value):
-        raise ImmutableError("This object is immutable")
+        if self.__freeze_attribute_assignment:
+            raise ImmutableError("This object is immutable")
+        else:
+            return super().__setattr__(name, value)
+
+    def __setitem__(self, name, value):
+        if self.__freeze_item_assignment:
+            raise ImmutableError("This object is immutable")
+        else:
+            return super().__setattr__(name, value)
 
 
-def freeze(obj: object):
+def freeze(obj: object, *, freeze_attribute_assignment=True, freeze_item_assignment=True, recursive=True):
     obj_type = obj.__class__
-    frozen_type = type(f"Frozen{obj_type.__name__}", (Frozen, obj_type), dict(obj_type.__dict__))
+    frozen_type = type(f"Frozen{obj_type.__name__}",
+                       (Frozen, obj_type),
+                       {"_Frozen__freeze_attribute_assignment": freeze_attribute_assignment,
+                        "_Frozen__freeze_item_assignment": freeze_item_assignment})
     frozen_type.__repr__ = lambda self: f"<Frozen({obj_type.__repr__(self)})>"
 
     obj.__class__ = frozen_type
--- tests/test_freeze.py
+++ tests/test_freeze.py
@@ -5,28 +5,75 @@ import pytest
 from cryo import freeze, ImmutableError
 
 
-def test_freeze():
-
+@pytest.fixture(scope='module')
+def dummy_class():
+    """Fixture that provides a simple dummy class with a value and repr."""
 
     class Dummy:
 
         def __init__(self, value):
             self.value = value
+            self._list = [1, 2, 3]
 
         def __repr__(self):
             return f"Dummy('value={self.value}')"
 
-    d = Dummy('hello')
+        def __getitem__(self, item):
+            return self._list[item]
+
+        def __setitem__(self, key, value):
+            self._list[key] = value
+
+    return Dummy
+
+
+def test_freeze(dummy_class):
+    """
+    Tests the following aspects of :func:`cryo.freeze`:
+        - Returns a reference to the frozen object.
+        - The original object and the one returned are the same
+        - The repr of the frozen object is correctly modified to indicate that the instance is frozen
+        - Instance checks with the original class still work on the frozen object
+    """
+
+    d = dummy_class('hello')
     d2 = deepcopy(d)
     frozen_d = freeze(d)
     assert frozen_d is d
-    with pytest.raises(ImmutableError):
-        d.value = 'world'
-    assert d.value == "hello"
     d_repr = repr(d).strip("\"\'")
+    assert d_repr != repr(d2)
     assert d_repr.startswith('<Frozen(')
     assert d_repr.endswith(')>')
     assert repr(d2).strip("\"\'") == d_repr.removeprefix("<Frozen(").removesuffix(")>")
 
-    pytest.xfail("Instance checks do not work yet")
-    assert isinstance(d, Dummy)
+    assert isinstance(d, dummy_class)
+
+
+def test_freeze_attribute_assignment(dummy_class):
+
+    d = dummy_class('hello')
+    freeze(d)
+    with pytest.raises(ImmutableError):
+        d.value = 'world'
+    assert d.value == "hello"
+
+
+def test_freeze_item_assignment(dummy_class):
+    d = dummy_class("Hi")
+    assert d[0] == 1
+    d[0] = 9001
+    assert d[0] == 9001
+
+    freeze(d)
+    with pytest.raises(ImmutableError):
+        d[0] = 1
+    assert d[0] == 9001
+
+@pytest.mark.xfail(reason="freezing builtins does not work")
+def test_freeze_item_assignment_builtins():
+
+    l = [1,2,3]
+    freeze(l)
+    with pytest.raises(ImmutableError):
+        l[0] = 5
+    assert l[0] == 1
