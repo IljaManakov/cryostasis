7b74d03402df0dd243d505e83612b892bbf69325
Ilja Manakov <ilja.manakov@gmx.de>
2024-10-12 10:26:53 +0200

    add recursive freezing and fix frozen set repr

--- src/cryostasis/__init__.py
+++ src/cryostasis/__init__.py
@@ -1,11 +1,18 @@
 __all__ = ["ImmutableError", "freeze"]
 
+from typing import TypeVar
+
 
 class ImmutableError(Exception):
     pass
 
 
-def freeze(obj: object, *, freeze_attributes=True, freeze_items=True):
+Instance = TypeVar("Instance", bound=object)
+
+
+def freeze(
+    obj: Instance, *, freeze_attributes: bool = True, freeze_items: bool = True
+) -> Instance:
     from .detail import _create_dynamic_frozen_type, IMMUTABLE_TYPES, Frozen
     from ._builtin_helpers import _set_class_on_builtin
 
@@ -19,3 +26,52 @@ def freeze(obj: object, *, freeze_attributes=True, freeze_items=True):
     else:
         obj.__class__ = frozen_type
     return obj
+
+
+def deepfreeze(
+    obj: Instance, *, freeze_attributes: bool = True, freeze_items: bool = True
+) -> Instance:
+    from itertools import chain
+
+    # set for keeping id's of seen instances
+    # we only keep the id's because some instances might not be hashable
+    # also we don't want to hold refs to the instances here and weakref is not supported by all types
+    seen_instances: set[int] = set()
+
+    def _deepfreeze(obj, freeze_attributes, freeze_items):
+        if id(obj) not in seen_instances:
+            seen_instances.add(id(obj))
+        else:
+            return obj
+
+        freeze(obj, freeze_attributes=freeze_attributes, freeze_items=freeze_items)
+
+        # freeze all attributes
+        try:
+            attr_iterator = vars(obj).values()
+        except TypeError:
+            pass
+        else:
+            for attr in attr_iterator:
+                _deepfreeze(attr, freeze_attributes, freeze_items)
+
+        if isinstance(obj, str):
+            return obj
+
+        # freeze all items
+        try:
+            item_iterator = iter(obj)
+            if isinstance(obj, dict):
+                item_iterator = chain(item_iterator, obj.values())
+        except TypeError:
+            pass
+        else:
+            for item in item_iterator:
+                _deepfreeze(item, freeze_attributes, freeze_items)
+
+        return obj
+
+    return _deepfreeze(obj, freeze_attributes, freeze_items)
+
+
+del TypeVar, Instance
